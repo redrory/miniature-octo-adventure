59c80350ca031ae1b49e0f8c058598c2cd17003c
Rory Walker <redrory@gmail.com>
2012-06-29 10:34:45 -0500

    Update email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,16 @@ Chukka::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'google.com',
+  :user_name            => 'chukkasm',
+  :password             => 'Chukka$m',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
