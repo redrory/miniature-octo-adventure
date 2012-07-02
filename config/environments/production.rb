2e7721f21a1408efc0302645da110e9b44e90da6
Rory Walker <redrory@gmail.com>
2012-07-02 14:32:10 -0500

    conflict

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Chukka::Application.configure do
 
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :address              => "smtp.chukkacaribbean.com",
+  :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'mail.gmail.com',
   :user_name            => 'chukkasm@gmail.com',
