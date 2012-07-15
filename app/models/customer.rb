998c9c941148ebb73929179384698a1bf084c978
Rory Walker <redrory@gmail.com>
2012-07-14 23:00:03 -0500

    remove placeholder

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ActiveRecord::Base
 	attr_accessible :tour_date,  :email, :price, :tour_name, :tour_location
-	validates :email, :presence => true
+ validates :email, :presence => true
 
 end
