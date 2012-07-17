388dfe2488a55b5d2f3320272b9c3d4906421865
Rory Walker <redrory@gmail.com>
2012-07-17 12:56:49 -0500

    Update email valid

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,16 @@
 class CustomersController < ApplicationController
   def create
+    
   	@customer = Customer.create params[:customer]
+  	if @customer.save
   	UserMailer.request_email(@customer).deliver
-  	#eedirect_to :back
+  	#flash[:success] = ""
+    #render :create
+  	else
+      redirect_to :back
+      flash[:success] = "Please enter a valid email"
+  	end
   	#render :text => params.inspect
   end
-
+  	
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,10 @@
 class Customer < ActiveRecord::Base
 	attr_accessible :tour_date,  :email, :price, :tour_name, :tour_location
- validates :email, :presence => true
+ #validates :email, presence: true 
+ email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, :format     => { :with => email_regex },
+                    :presence => true,
+                    :length => { :within => 5..50 }
 
 end
